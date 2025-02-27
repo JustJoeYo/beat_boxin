285480b24bac3fb5b66eda75d16351b28def3851
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-02-27 12:43:02 -0800

    iteration 1 completed

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe LinkedList do
 
   describe "initialization" do
     it "#initialize" do
-      expect(@list).to be_an_instance_of(LinkedList)
+      expect(@list).to be_an_instance_of(LinkedList) # list = LinkedList.new
     end
 
     it '#attributes' do
@@ -22,17 +22,28 @@ RSpec.describe LinkedList do
   describe 'methods' do
     it '#append' do
       @list.append('doop')
-      expect(@list.head.data).to eq('doop')
+      expect(@list.head.data).to eq('doop') 
+      expect(@list.head.next_node).to eq(nil)
+
+      @list.append('deep')
+      expect(@list.head.next_node.data).to eq('deep')
+      expect(@list.head.next_node.next_node).to eq(nil)
     end
 
     it '#count' do
       @list.append('doop')
       expect(@list.count).to eq(1)
+
+      @list.append('deep')
+      expect(@list.count).to eq(2)
     end
 
     it '#to_string' do
       @list.append('doop')
       expect(@list.to_string).to eq('doop')
+
+      @list.append('deep')
+      expect(@list.to_string).to eq('doop deep')
     end
   end
 end
